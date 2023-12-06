8c33fe7543857d3234e8e10d833136edde451ce3
Ilias Karim <ilias.karim@gmail.com>
2023-12-06 18:26:47 -0500

    root

---
+++ File Templates/SwiftUI View.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+/// <#Description#>
+struct ___FILEBASENAMEASIDENTIFIER___: View {
+  var body: some View {
+    VStack {
+      Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }.padding()
+  }
+}
+
+#Preview {
+  ___FILEBASENAMEASIDENTIFIER___()
+}
